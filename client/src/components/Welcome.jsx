46ede423357c4d7f12dfd99140d8986008971dc2
Bartosz Koscielniak <bk113737@stud.ur.edu.pl>
2022-06-08 20:31:56 +0200

    ADD: connecting with metamask wallet

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,45 @@
+import React, {useContext} from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { TransactionContext } from "../context/TransactionContext";
+import { Loader } from "./";
+
 const Welcome = () => {
+    const {connectWallet, currentAccount} = useContext(TransactionContext);
+
+    //console.log(value);
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <h1>Welcome</h1>
+        <div className="flex w-full justify-center items-center">
+        <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+          <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+            <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+              Send Crypto <br /> across the world
+            </h1>
+            <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+              Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+            </p>
+            {!currentAccount && (
+              <button
+                type="button"
+                onClick={connectWallet}
+                className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+              >
+                <AiFillPlayCircle className="text-white mr-2" />
+                <p className="text-white text-base font-semibold">
+                  Connect Wallet
+                </p>
+              </button>
+            )}
+            </div>
+            </div>
+            </div>
     );
 }
 
