66eb2b564f50e8e4f6dea729c4af3db5d3f4bc6a
Bartosz Koscielniak <bk113737@stud.ur.edu.pl>
2022-06-10 01:51:29 +0200

    ADD: Footer.jsx

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/wRMli82yoNDMSnnnqGD90Xi3ti778o1L',
-      accounts: [ '716da35b19c81304b6eaf5d5b94c69cdde762c6be978817613fc8e05216c6d03' ]
+      url: 'https://eth-ropsten.alchemyapi.io/v2/wRMli82yoNDMSnnnqGD90Xi3ti778o1L',// test environment
+      accounts: [ '716da35b19c81304b6eaf5d5b94c69cdde762c6be978817613fc8e05216c6d03' ]//private key of contract funding wallet 
     }
   }
 }
