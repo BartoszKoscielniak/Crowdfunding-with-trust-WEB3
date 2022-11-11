d2c82a781320763d929b202e41df64d521082c60
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2022-11-11 20:20:20 +0100

    CHG: removed deprecated WebSecurityConfigurerAdapter

--- crowdfunding-api/src/main/java/com/crowdfunding/crowdfundingapi/config/security/WebSecurityConfiguration.java
+++ crowdfunding-api/src/main/java/com/crowdfunding/crowdfundingapi/config/security/WebSecurityConfiguration.java
@@ -5,63 +5,48 @@ import com.crowdfunding.crowdfundingapi.user.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration {
 
     private final PasswordConfig passwordEncoder;
     private final UserService userService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()//Use in browser apps, disable in non-browser apps
-                .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .addFilter(new AuthenticationFilter(authenticationManager()))
-                .addFilterAfter(new AuthorizationFilter(), AuthenticationFilter.class)
-                .authorizeRequests()
-                .antMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()//can make index page to be seen by everyone
-                .anyRequest()
-                .authenticated();
-                /*.and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .defaultSuccessUrl("/home", true)
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))//DELETE when csrf is enabled cuz only POST is available then
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessUrl("/login");*/
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder.passwordEncoder());
-        provider.setUserDetailsService(userService);
-        return provider;
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder.passwordEncoder());
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        http
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new AuthenticationFilter(authenticationManager))
+                .addFilterAfter(new AuthorizationFilter(), AuthenticationFilter.class)
+                .authenticationManager(authenticationManager)
+                .authorizeRequests()
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                .anyRequest()
+                .authenticated();
+
+        return http.build();
     }
+
 }
