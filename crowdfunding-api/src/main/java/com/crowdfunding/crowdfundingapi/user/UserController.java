1b4ff17bd0ebfd9a2d1ef79f4c2b72512ccd9b8e
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2022-11-20 20:09:05 +0100

    ADD: nonce endpoint

--- crowdfunding-api/src/main/java/com/crowdfunding/crowdfundingapi/user/UserController.java
+++ crowdfunding-api/src/main/java/com/crowdfunding/crowdfundingapi/user/UserController.java
@@ -1,4 +1,33 @@
 package com.crowdfunding.crowdfundingapi.user;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/user")
+@AllArgsConstructor
 public class UserController {
+    /*
+    *  TODO: historia transakcji
+    *  TODO: informacje o uzytkowniku
+    *  TODO: przelewanie srodkow
+    *  TODO: przechowywanie srodkow
+    *  TODO: */
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping(path = "/nonce")
+    public Optional<String> getUsersNonce(
+            @RequestParam String publicAddress,
+            @RequestParam String password){
+
+        return userService.getUsersNonce(publicAddress, password);
+    }
 }
