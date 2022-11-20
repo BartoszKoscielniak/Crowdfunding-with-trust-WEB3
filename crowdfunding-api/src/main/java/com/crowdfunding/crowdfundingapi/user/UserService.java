d5b268e04d8ef39bc167b07da85f0afcbdf77249
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2022-11-20 20:09:31 +0100

    ADD: user services

--- crowdfunding-api/src/main/java/com/crowdfunding/crowdfundingapi/user/UserService.java
+++ crowdfunding-api/src/main/java/com/crowdfunding/crowdfundingapi/user/UserService.java
@@ -1,22 +1,56 @@
 package com.crowdfunding.crowdfundingapi.user;
 
+import com.crowdfunding.crowdfundingapi.config.PasswordConfig;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UserService implements UserDetailsService {
 
     private UserRepository userRepository;
+    private final PasswordConfig passwordConfig;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return userRepository.findUserByPublicKey(username)
+        return userRepository.findUserByPublicAddress(username)
                 .orElseThrow(() ->
                     new UsernameNotFoundException(String.format("User: %s not found", username))
                 );
+    }
+
+    public List<User> getAllUsers( ) {
+        return userRepository.findAll();
+    }
+
+    public Optional<String> getUsersNonce(String publicAddress, String password) {
+        Optional<User> user = userRepository.findUserByPublicAddress(publicAddress);
+        if (user.isPresent()){
+            if (passwordConfig.passwordEncoder().matches(password ,user.get().getPassword())){
+                return Optional.ofNullable(user.get().getNonce());
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<User> getUserByPublicAddress(String publicAddress){
+        return userRepository.findUserByPublicAddress(publicAddress);
+    }
+
+    public void updateUser(User user){
+        userRepository.save(user);
+    }
+
+    public User getUserFromAuthentication( ){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findUserByPublicAddress(authentication.getName()).get();
     }
 }
