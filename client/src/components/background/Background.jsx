a03fa5835c92e6fc7704a7e1d0af2588d52b65de
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2023-01-08 22:45:44 +0100

    ADD: Background.jsx

--- client/src/components/background/Background.jsx
+++ client/src/components/background/Background.jsx
@@ -1,8 +1,8 @@
 const Background = () => {
     return (
-        <div className="bg-zinc-900 object-contain w-full h-full relative">
+        <div className="bg-zinc-800 bg-cover object-contain w-full h-full relative">
             <div className="absolute w-[250px] h-[250px] top-1/4 left-1/4 animate-rotation">
-                <div className="absolute bg-violet-500 w-[650px] h-[650px] top-[-10px] left-[-10px] rounded-full blur-[100px] animate-flashGradient" />
+                <div className="absolute bg-violet-900 w-[650px] h-[650px] top-[-10px] left-[-10px] rounded-full blur-[100px] animate-flashGradient" />
             </div>
 
             <div className="absolute w-[250px] h-[250px] top-[55%] left-[55%] animate-rotationv2">
