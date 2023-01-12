f7a2cff48ad4bd76265851bbce8bf39da19e7214
Bartosz Koscielniak <bartoszkoscielniak3@gmail.com>
2023-01-12 02:01:23 +0100

    CHG: relation update

--- crowdfunding-api/src/main/java/com/crowdfunding/crowdfundingapi/support/RelationRepository.java
+++ crowdfunding-api/src/main/java/com/crowdfunding/crowdfundingapi/support/RelationRepository.java
@@ -1,5 +1,6 @@
 package com.crowdfunding.crowdfundingapi.support;
 
+import com.crowdfunding.crowdfundingapi.collection.Collection;
 import com.crowdfunding.crowdfundingapi.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,9 +13,12 @@ public interface RelationRepository extends JpaRepository<CollUserRelation, Long
     @Query("SELECT r FROM CollUserRelation r WHERE r.user.id = ?1 AND r.collectionRelation.id = ?2")
     Optional<CollUserRelation> findRelationByUseridCollectionid(Long userId, Long collectionId);
 
+    @Query("SELECT r FROM CollUserRelation r WHERE r.user.id = ?1 AND r.phase.id = ?2")
+    Optional<CollUserRelation> findRelationByUseridPhaseid(Long userId, Long phaseId);
+
     @Query("SELECT r FROM CollUserRelation r WHERE r.user.id = ?1")
     List<CollUserRelation> findRelationByUserid(Long userId);
 
-    @Query("SELECT r FROM CollUserRelation r WHERE r.collectionRelation.id = ?1")
-    List<CollUserRelation> findRelationByCollectionid(Long collectionId);
+    @Query("SELECT c FROM CollUserRelation r, Collection c WHERE r.type = ?1 AND r.user.publicAddress = ?2 AND r.collectionRelation.id = c.id")
+    List<Collection> findCollectionsByRelation(CollUserType type, String publicAddress);
 }
